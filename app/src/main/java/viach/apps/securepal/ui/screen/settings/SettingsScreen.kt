bb6ad71fb5a36a7d514e9512ce7495645c305eff
Viacheslav Chugunov <viacheslav.chugunov@protonmail.com>
2023-09-30 16:57:40 +0300

    New ui for settings

--- app/src/main/java/viach/apps/securepal/ui/screen/settings/SettingsScreen.kt
+++ app/src/main/java/viach/apps/securepal/ui/screen/settings/SettingsScreen.kt
@@ -1,5 +1,6 @@
 package viach.apps.securepal.ui.screen.settings
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,11 +11,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -37,53 +40,77 @@ fun SettingsScreen(
                 .fillMaxSize()
                 .verticalScroll(rememberScrollState())
         ) {
-            Text(
-                text = stringResource(id = R.string.app_settings),
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.titleLarge
-            )
-            Spacer(modifier = Modifier.height(8.dp))
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .clickable { onAction(SettingsAction.ShowAppThemeBottomSheet(true)) }
                     .padding(all = 16.dp)
+                    .background(
+                        color = MaterialTheme.colorScheme.primaryContainer,
+                        shape = CutCornerShape(16.dp)
+                    )
             ) {
+                Spacer(modifier = Modifier.height(16.dp))
                 Text(
-                    text = stringResource(id = R.string.theme),
-                    style = MaterialTheme.typography.titleMedium
+                    text = stringResource(id = R.string.app_settings),
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.titleLarge
                 )
-                Spacer(modifier = Modifier.height(2.dp))
-                Text(
-                    text = stringResource(id = when (state.appTheme) {
-                        AppTheme.LIGHT -> R.string.light
-                        AppTheme.DARK -> R.string.dark
-                        AppTheme.SYSTEM -> R.string.system
-                        AppTheme.DYNAMIC_LIGHT -> R.string.dynamic_light
-                        AppTheme.DYNAMIC_DARK -> R.string.dynamic_dark
-                        AppTheme.DYNAMIC_SYSTEM -> R.string.dynamic_system
-                    })
-                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(CutCornerShape(bottomEnd = 16.dp, bottomStart = 16.dp))
+                        .clickable { onAction(SettingsAction.ShowAppThemeBottomSheet(true)) }
+                        .padding(all = 16.dp)
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.theme),
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    Spacer(modifier = Modifier.height(2.dp))
+                    Text(
+                        text = stringResource(id = when (state.appTheme) {
+                            AppTheme.LIGHT -> R.string.light
+                            AppTheme.DARK -> R.string.dark
+                            AppTheme.SYSTEM -> R.string.system
+                            AppTheme.DYNAMIC_LIGHT -> R.string.dynamic_light
+                            AppTheme.DYNAMIC_DARK -> R.string.dynamic_dark
+                            AppTheme.DYNAMIC_SYSTEM -> R.string.dynamic_system
+                        })
+                    )
+                }
             }
-            Spacer(modifier = Modifier.height(24.dp))
-            Text(
-                text = stringResource(id = R.string.additional),
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.titleLarge
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = stringResource(id = R.string.privacy_policy),
+            Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .clickable { context.openLink("https://docs.google.com/document/d/1P2vxnY85dxue9nPfDGmUfKFuq3LKnHEhCgDtXpJ60MQ/edit") }
-                    .padding(all = 16.dp)
-            )
+                    .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+                    .background(
+                        color = MaterialTheme.colorScheme.secondaryContainer,
+                        shape = CutCornerShape(16.dp)
+                    )
+            ) {
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = stringResource(id = R.string.additional),
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = stringResource(id = R.string.privacy_policy),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(CutCornerShape(bottomEnd = 16.dp, bottomStart = 16.dp))
+                        .clickable { context.openLink("https://docs.google.com/document/d/1P2vxnY85dxue9nPfDGmUfKFuq3LKnHEhCgDtXpJ60MQ/edit") }
+                        .padding(all = 16.dp)
+                )
+            }
         }
         AppThemeBottomSheet(
             show = state.showAppThemeBottomSheet,
+            selectedAppTheme = state.appTheme,
             onDismiss = { onAction(SettingsAction.ShowAppThemeBottomSheet(false)) },
             onAppThemeSelected = { onAction(SettingsAction.SetAppTheme(it)) }
         )
--- app/src/main/java/viach/apps/securepal/ui/view/AppThemeBottomSheet.kt
+++ app/src/main/java/viach/apps/securepal/ui/view/AppThemeBottomSheet.kt
@@ -1,23 +1,19 @@
 package viach.apps.securepal.ui.view
 
 import android.os.Build
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.launch
 import viach.apps.securepal.R
 import viach.apps.storage.model.AppTheme
 
@@ -25,13 +21,13 @@ import viach.apps.storage.model.AppTheme
 @Composable
 fun AppThemeBottomSheet(
     show: Boolean,
+    selectedAppTheme: AppTheme,
     onDismiss: () -> Unit,
     onAppThemeSelected: (AppTheme) -> Unit
 ) {
     if (!show) return
 
     val stateSheet = rememberModalBottomSheetState(skipPartiallyExpanded = true)
-    val coroutineScope = rememberCoroutineScope()
 
     ModalBottomSheet(
         onDismissRequest = onDismiss,
@@ -44,86 +40,62 @@ fun AppThemeBottomSheet(
             style = MaterialTheme.typography.titleMedium
         )
         Spacer(modifier = Modifier.height(8.dp))
-        Text(
-            text = stringResource(id = R.string.light),
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable {
-                    coroutineScope.launch {
-                        stateSheet.hide()
-                        onDismiss()
-                        onAppThemeSelected(AppTheme.LIGHT)
-                    }
-                }
-                .padding(16.dp)
+        AppThemeBottomSheetItem(
+            titleRes = R.string.light,
+            isSelected = selectedAppTheme == AppTheme.LIGHT,
+            onClick = {
+                stateSheet.hide()
+                onDismiss()
+                onAppThemeSelected(AppTheme.LIGHT)
+            }
         )
-        Text(
-            text = stringResource(id = R.string.dark),
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable {
-                    coroutineScope.launch {
-                        stateSheet.hide()
-                        onDismiss()
-                        onAppThemeSelected(AppTheme.DARK)
-                    }
-                }
-                .padding(16.dp)
+        AppThemeBottomSheetItem(
+            titleRes = R.string.dark,
+            isSelected = selectedAppTheme == AppTheme.DARK,
+            onClick = {
+                stateSheet.hide()
+                onDismiss()
+                onAppThemeSelected(AppTheme.DARK)
+            }
         )
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            Text(
-                text = stringResource(id = R.string.system),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clickable {
-                        coroutineScope.launch {
-                            stateSheet.hide()
-                            onDismiss()
-                            onAppThemeSelected(AppTheme.SYSTEM)
-                        }
-                    }
-                    .padding(16.dp)
+            AppThemeBottomSheetItem(
+                titleRes = R.string.system,
+                isSelected = selectedAppTheme == AppTheme.SYSTEM,
+                onClick = {
+                    stateSheet.hide()
+                    onDismiss()
+                    onAppThemeSelected(AppTheme.SYSTEM)
+                }
             )
         }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            Text(
-                text = stringResource(id = R.string.dynamic_light),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clickable {
-                        coroutineScope.launch {
-                            stateSheet.hide()
-                            onDismiss()
-                            onAppThemeSelected(AppTheme.DYNAMIC_LIGHT)
-                        }
-                    }
-                    .padding(16.dp)
+            AppThemeBottomSheetItem(
+                titleRes = R.string.dynamic_light,
+                isSelected = selectedAppTheme == AppTheme.DYNAMIC_LIGHT,
+                onClick = {
+                    stateSheet.hide()
+                    onDismiss()
+                    onAppThemeSelected(AppTheme.DYNAMIC_LIGHT)
+                }
             )
-            Text(
-                text = stringResource(id = R.string.dynamic_dark),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clickable {
-                        coroutineScope.launch {
-                            stateSheet.hide()
-                            onDismiss()
-                            onAppThemeSelected(AppTheme.DYNAMIC_DARK)
-                        }
-                    }
-                    .padding(16.dp)
+            AppThemeBottomSheetItem(
+                titleRes = R.string.dynamic_dark,
+                isSelected = selectedAppTheme == AppTheme.DYNAMIC_DARK,
+                onClick = {
+                    stateSheet.hide()
+                    onDismiss()
+                    onAppThemeSelected(AppTheme.DYNAMIC_DARK)
+                }
             )
-            Text(
-                text = stringResource(id = R.string.dynamic_system),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clickable {
-                        coroutineScope.launch {
-                            stateSheet.hide()
-                            onDismiss()
-                            onAppThemeSelected(AppTheme.DYNAMIC_SYSTEM)
-                        }
-                    }
-                    .padding(16.dp)
+            AppThemeBottomSheetItem(
+                titleRes = R.string.dynamic_system,
+                isSelected = selectedAppTheme == AppTheme.DYNAMIC_SYSTEM,
+                onClick = {
+                    stateSheet.hide()
+                    onDismiss()
+                    onAppThemeSelected(AppTheme.DYNAMIC_SYSTEM)
+                }
             )
         }
         Spacer(modifier = Modifier.height(24.dp))
