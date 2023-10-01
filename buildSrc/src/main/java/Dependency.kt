f6748756c426a38fcdaae7f673627b3ad16e5b07
Viacheslav Chugunov <viacheslav.chugunov@protonmail.com>
2023-10-01 19:23:48 +0300

    dev branch

--- buildSrc/src/main/java/Dependency.kt
+++ buildSrc/src/main/java/Dependency.kt
@@ -8,8 +8,8 @@ object Dependency {
         const val APPLICATION_ID = "viach.apps.securepal"
         const val MIN_SDK = 21
         const val TARGET_SDK = 34
-        const val VERSION_CODE = 1
-        const val VERSION_NAME = "1.0.0"
+        const val VERSION_CODE = 2
+        const val VERSION_NAME = "1.0.1"
     }
 
     object Plugin {
